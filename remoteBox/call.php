09f2f89259d6464d4480657ad6ad564a0aed0a02
geraskalnas <geraskalnas@gmailcom>
2018-06-15 19:18:24 +0300

    Update

--- remoteBox/call.php
+++ remoteBox/call.php
@@ -1,4 +1,7 @@
 <?php
 $output='{"content": "Nothing that can interest you","buttons": [{"name": "fdhhdfd", "id": "1", "action": "report", "data": "yt"}, {"name": "fdgs", "id":"2", "action": "alert", "data": "dsf"}]}';
+
+if(isset($_GET["button"]) and $_GET["button"]==1){$output='{"content": "Changed", "buttons":[]}';}
+
 echo $output;
 ?>
